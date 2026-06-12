21ae7fd13c71406db26b8130e03a6808ac11ece9
ESTEBAN VASQUEZ <estebanjvasquez@gmail.com>
2026-06-12 12:02:26 +0200

    diag: reportar config de BD (host/user/name + longitud de pass) para depurar 1045

--- api/diag.php
+++ api/diag.php
@@ -13,9 +13,18 @@ if (!(is_admin() || ($secret !== '' && hash_equals($secret, (string)$token)))) {
     json_out(['ok' => false, 'error' => 'No autorizado. Use ?token=CRON_SECRET o inicie sesión como admin.'], 403);
 }
 
+$dbc = $GLOBALS['CONFIG']['db'];
 $report = [
     'php_version' => PHP_VERSION,
     'db_connected' => false,
+    'db_config' => [
+        'host' => $dbc['host'],
+        'name' => $dbc['name'],
+        'user' => $dbc['user'],
+        'pass_len' => strlen((string)($dbc['pass'] ?? '')),
+        'pass_is_placeholder' => (($dbc['pass'] ?? '') === 'PON_AQUI_LA_CONTRASEÑA_DE_MYSQL'),
+        'config_file' => realpath(__DIR__ . '/config.php'),
+    ],
     'tables' => [],
     'gd_webp' => function_exists('imagewebp'),
     'uploads_dir' => $GLOBALS['CONFIG']['paths']['uploads_dir'],
